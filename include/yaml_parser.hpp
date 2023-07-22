41d7d3a0e439046694fe255eb71d6201787b91cb
jdiestra <julius.diestra.cabrera@gmail.com>
2023-07-22 10:10:53 +0200

    Fix for clang format rules

--- include/yaml_parser.hpp
+++ include/yaml_parser.hpp
@@ -2,15 +2,17 @@
 #ifndef YAML_PARSER_HPP_
 #define YAML_PARSER_HPP_
 
-#include "yaml.h"
 #include <iostream>
 #include <string>
+
+#include "yaml.h"
 
 class YamlParser {
   public:
     YamlParser();
     ~YamlParser();
     bool Read(const char* file_path);
+
   private:
     yaml_parser_t parser_;
     yaml_event_t event_;
--- src/yaml_parser.cpp
+++ src/yaml_parser.cpp
@@ -10,7 +10,7 @@ YamlParser::~YamlParser() {
 }
 
 bool YamlParser::Read(const char* file_path) {
-    FILE* file_ = fopen(file_path,"rb");
+    FILE* file_ = fopen(file_path, "rb");
     bool open_file_status{false};
     if (file_ != nullptr) {
         open_file_status = true;
